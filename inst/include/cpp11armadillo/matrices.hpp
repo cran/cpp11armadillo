109ad98641336b4fc357d8685cc0b028581e3a07
Mauricio Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-07-04 21:00:02 +0000

    version 0.2.8

--- inst/include/cpp11armadillo/matrices.hpp
+++ inst/include/cpp11armadillo/matrices.hpp
@@ -2,7 +2,6 @@
 
 using namespace arma;
 using namespace cpp11;
-using namespace std;
 
 #ifndef MATRICES_HPP
 #define MATRICES_HPP
@@ -14,26 +13,37 @@ using namespace std;
 template <typename T>
 inline Mat<T> as_Mat(const T& x) {
   // Generic implementation
-  throw runtime_error("Cannot convert to Mat");
+  throw std::runtime_error("Cannot convert to Mat");
 }
 
 template <typename T, typename U>
 inline Mat<T> dblint_matrix_to_Mat_(const U& x) {
-  int n = x.nrow();
-  int m = x.ncol();
-  Mat<T> B(
-      (is_same<U, doubles_matrix<>>::value ? reinterpret_cast<T*>(REAL(x.data()))
-                                           : reinterpret_cast<T*>(INTEGER(x.data()))),
-      n, m, false, false);
-  return B;
+  const int n = x.nrow();
+  const int m = x.ncol();
+
+  Mat<T> y(n, m);
+
+  if (std::is_same<U, doubles_matrix<>>::value) {
+    y = Mat<T>(reinterpret_cast<T*>(REAL(x.data())), n, m, false, false);
+  } else {
+    y = Mat<T>(reinterpret_cast<T*>(INTEGER(x.data())), n, m, false, false);
+  }
+
+  return y;
 }
 
 template <typename T, typename U>
 inline Mat<T> dblint_to_Mat_(const U& x) {
-  int n = x.size();
-  Mat<T> y((is_same<U, doubles>::value ? reinterpret_cast<T*>(REAL(x.data()))
-                                       : reinterpret_cast<T*>(INTEGER(x.data()))),
-           n, 1, false, false);
+  const int n = x.size();
+
+  Mat<T> y(n, 1);
+
+  if (std::is_same<U, doubles>::value) {
+    y = Mat<T>(reinterpret_cast<T*>(REAL(x.data())), n, 1, false, false);
+  } else {
+    y = Mat<T>(reinterpret_cast<T*>(INTEGER(x.data())), n, 1, false, false);
+  }
+
   return y;
 }
 
@@ -57,20 +67,21 @@ inline Mat<int> as_Mat(const integers& x) { return dblint_to_Mat_<int, integers>
 
 template <typename T, typename U>
 inline U Mat_to_dblint_matrix_(const Mat<T>& A) {
-  int n = A.n_rows;
-  int m = A.n_cols;
+  const int n = A.n_rows;
+  const int m = A.n_cols;
 
-  typename conditional<is_same<U, doubles_matrix<>>::value, writable::doubles_matrix<>,
-                       writable::integers_matrix<>>::type B(n, m);
+  using dblint_matrix =
+      typename std::conditional<std::is_same<U, doubles_matrix<>>::value,
+                           writable::doubles_matrix<>, writable::integers_matrix<>>::type;
+
+  dblint_matrix B(n, m);
 
 #ifdef _OPENMP
-#pragma omp parallel for collapse(2)
+#pragma omp parallel for collapse(2) schedule(static)
 #endif
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < m; ++j) {
-      typename conditional<is_same<U, doubles_matrix<>>::value, double, int>::type a_ij =
-          A(i, j);
-      B(i, j) = a_ij;
+      B(i, j) = A(i, j);
     }
   }
 
@@ -89,7 +100,6 @@ inline integers_matrix<> as_integers_matrix(const Mat<int>& A) {
 
 template <typename T>
 inline list Mat_to_complex_matrix_(const Mat<T>& A) {
-  static_assert(is_same<T, complex<double>>::value, "T must be complex<double>");
   Mat<double> A_real = real(A);
   Mat<double> A_imag = imag(A);
 
@@ -100,8 +110,8 @@ inline list Mat_to_complex_matrix_(const Mat<T>& A) {
   return B;
 }
 
-inline list as_complex_matrix(const Mat<complex<double>>& A) {
-  return Mat_to_complex_matrix_<complex<double>>(A);
+inline list as_complex_matrix(const Mat<std::complex<double>>& A) {
+  return Mat_to_complex_matrix_<std::complex<double>>(A);
 }
 
 #endif
